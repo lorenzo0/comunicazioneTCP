628f825f1eac4da2d398782139e4f6392c9659e6
albertomargheriti <alberto9959@gmail.com>
2018-02-12 10:42:23 +0100

    Aggiunta metodi mancano commenti

--- src/GestioneMessaggio.java
+++ src/GestioneMessaggio.java
@@ -1,5 +1,9 @@
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -26,14 +30,54 @@ public class GestioneMessaggio {
     
     public void inLinea(ServerSocket sS)
     {
+        if(sS.isClosed() != true)
+        {
+            System.out.println("L'utente è in linea...");
+        }
+    }
+    
+    public void nonInLinea(ServerSocket sS)
+    {
+        if(sS.isClosed() == true)
+        {
+            System.out.println("L'utente non è in linea...");
+        }
+    }
+    
+    public void echo(DataInputStream dis, Socket connection) throws IOException
+    {
+        String stringaLetta;
+        stringaLetta = dis.readUTF();
+        
+        DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
+        dos.writeUTF(stringaLetta);
+        dos.flush();
+    }
+    
+    public void end(ServerSocket sS) throws IOException
+    {
+        sS.close();
         
         if(sS.isClosed() == true)
         {
-            System.out.println("Connessione chiusa");
+            System.out.println("La connessione è chiusa...");
         }
         else
         {
-            System.out.println("Connessione aperta");
+            System.out.println("La connessione non è stata chiusa, server in ascolto...");
         }
+    }
+    
+    public void smile(DataInputStream dis, Socket connection) throws IOException
+    {
+        String stringaLetta;
+        String smile = ":)";
+        stringaLetta = dis.readUTF();
+        String stringaDaInviare = "";
+        
+//        if(stringaLetta.equals(smile))
+//        {
+//            stringaDaInviare = stringaDaInviare.replaceAll(":)", imageToAdd);
+//        }
     }
 }
